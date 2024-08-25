c1f683afd6c0bc49436da17f73aca8f50a05deb9
aitbek_312_ <askerovaitbek0607@gmail.com>
2024-08-25 11:29:45 +0600

    drfaer

---
+++ kino/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(UserProfile)
+admin.site.register(Country)
+admin.site.register(Director)
+admin.site.register(Actor)
+admin.site.register(Janre)
+admin.site.register(Movie)
+admin.site.register(Rating)
+admin.site.register(Comment)
